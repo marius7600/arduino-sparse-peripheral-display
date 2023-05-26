678cd919eaf477013c30348912bae214d33deeca
Marius Röhm <marius.roehm@gmail.com>
2023-05-26 22:07:56 +0200

    Switch back to high baud rate

--- src/main.cpp
+++ src/main.cpp
@@ -3,31 +3,39 @@
 
 #define NUM_LEDS 2  // Number of LEDs
 #define DATA_PIN 2  // Data pin for the LEDs
+#define BAUD_RATE 250000  // Baud rate for the serial connection
+// #define BAUD_RATE 19200  // Baud rate for the serial connection
+// #define BAUD_RATE 23000  // Baud rate for the serial connection
 
 CRGB leds[NUM_LEDS];
 bool newData = false;
 
 void setup() {
-  Serial.begin(250000);  // Start the serial connection (not working)
-  // Serial.begin(19200);  // Start the serial connection (working fine)
-  // Serial.begin(23000);  // Start the serial connection (working fine)
+  // Start the serial connection
+  Serial.begin(BAUD_RATE);
 
+  // Setup the LEDs
   FastLED.addLeds<WS2812B, DATA_PIN, GRB>(leds, NUM_LEDS);  // LED setup
   FastLED.setBrightness(10);  // Set the brightness of the LEDs
 }
 
 void loop() {
   if (Serial.available() >= NUM_LEDS * 3) {
+    // data is 3 bytes per LED
     byte data[NUM_LEDS * 3];
 
+    // Read the data from the serial connection
     Serial.readBytes(data, NUM_LEDS * 3);
 
+    // Set the LEDs to the data
     for (int i = 0; i < NUM_LEDS; i++) {
       byte r = data[i * 3];
       byte g = data[i * 3 + 1];
       byte b = data[i * 3 + 2];
       leds[i] = CRGB(r, g, b);
     }
+
+    // Push the data to the LEDs
     FastLED.show();
   }
 }
