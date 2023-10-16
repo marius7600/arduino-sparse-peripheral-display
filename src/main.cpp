8ff7eb10746951090c37ae753fb2e083f6a4b38c
marius7600 <56048471+marius7600@users.noreply.github.com>
2023-10-16 12:20:30 +0200

    comments and cleanup of main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,13 +3,10 @@
 
 #define NUM_LEDS 48  // Number of LEDs
 #define DATA_PIN 2  // Data pin for the LEDs
-// #define BAUD_RATE 350000  // Baud rate for the serial connection
 #define BAUD_RATE 256000  // Baud rate for the serial connection
-// #define BAUD_RATE 19200  // Baud rate for the serial connection
-// #define BAUD_RATE 23000  // Baud rate for the serial connection
 
-CRGB leds[NUM_LEDS];
-bool newData = false;
+CRGB leds[NUM_LEDS];  // LED array
+
 int currentLED = 0;
 bool initialized = false;
 
@@ -18,10 +15,11 @@ void setup() {
   Serial.begin(BAUD_RATE);
 
   // Setup the LEDs
-  FastLED.addLeds<WS2812B, DATA_PIN, GRB>(leds, NUM_LEDS);  // LED setup
-  FastLED.setBrightness(10);  // Set the brightness of the LEDs
+  FastLED.addLeds<WS2812B, DATA_PIN, GRB>(leds, NUM_LEDS);
+  FastLED.setBrightness(10);
 }
 
+/// @brief Receives data from the serial connection and pushes it to the LEDs
 void unity() {
   if (Serial.available() >= NUM_LEDS * 3) {
     // data is 3 bytes per LED
@@ -30,7 +28,7 @@ void unity() {
     // Read the data from the serial connection
     Serial.readBytes(data, NUM_LEDS * 3);
 
-    // Set the LEDs to the data
+    // Decode the data
     for (int i = 0; i < NUM_LEDS; i++) {
       byte r = data[i * 3];
       byte g = data[i * 3 + 1];
@@ -43,6 +41,7 @@ void unity() {
   }
 }
 
+/// @brief Turns on one LED at a time
 void singleLedTest () {
   // turn current LED on
   leds[currentLED] = CRGB::White;
@@ -54,6 +53,7 @@ void singleLedTest () {
   currentLED = (currentLED + 1) % NUM_LEDS;
 }
 
+/// @brief Turns on all LEDs
 void testAllLeds() {
   if (!initialized) {
     for (int i = 0; i < NUM_LEDS; i++) {
